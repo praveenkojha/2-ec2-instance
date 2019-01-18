56c248ae4db54b5948d91db12fa86931e88e6942
praveenkojha <praveenkkojha@gmail.com>
2019-01-18 15:25:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 
-resource "aws_instance" "my-test-instance" {
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "t2.micro"
-
-  tags {
-    Name = "test-instance"
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  depends_on = ["aws_s3_bucket.example"]
   }
-}
+resource "aws_s3_bucket" "example" {
+bucket = "terraform-demo000"
+  acl    = "private"
+  }
