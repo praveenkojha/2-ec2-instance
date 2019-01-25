09058efcf3b433c7efff1bf6b3cd909fe4fabf5a
praveenkojha <praveenkkojha@gmail.com>
2019-01-25 23:00:23 +0530

    addede subnet,igw

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-  depends_on = ["aws_s3_bucket.example"]
-  }
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-demo000"
-  acl    = "private"
-  }
+#resource "aws_instance" "example" {
+#  ami           = "ami-b374d5a5"
+#  instance_type = "tr2.micro"
+# depends_on       = ["aws_s3_bucket.example"]
+#tags {
+#  name = "test-instance"
+#  }
+#}
+
+#resource "aws_s3_bucket" "example" {
+#  bucket = "example-bucket"
+#  acl    = "private"
+#}
+
+#CIDR block for the VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.cidr_vpc}"
   enable_dns_support   = true
@@ -16,3 +22,23 @@ tags = {
  Name = "vpc-demo"
  }
 }
+
+#CIDR block for the SUBNET
+resource "aws_subnet" "subnet_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.cidr_subnet}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${var.availability_zone}"
+tags = {
+ Name = "vpc-demo-subnet-public"
+ }
+}
+ 
+#"internet gateway associated with VPC"
+resource "aws_internet_gateway"  "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+   Name = "cidr-igw" 
+  } 
+}
+
